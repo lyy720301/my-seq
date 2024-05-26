67a8ae66101c24f2ba0bf9848102cf2a531694f0
lyy720301 <444109668@qq.com>
2024-05-27 01:40:36 +0800

    更改连接数；处理手动操作datasource的bug：连接未释放

--- src/main/java/cn/lz/dao/SeqDao.java
+++ src/main/java/cn/lz/dao/SeqDao.java
@@ -1,55 +1,54 @@
 package cn.lz.dao;
 
 
+import cn.lz.conf.DynamicDataSource;
 import cn.lz.conf.DynamicDataSourceContextHolder;
 import cn.lz.service.RoutingStrategy;
 import cn.lz.service.RoutingStrategyFactory;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.PreparedStatementCreator;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
 
 import java.sql.Connection;
-import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 @Slf4j
 @Component
 public class SeqDao {
 
-    @Resource
-    private JdbcTemplate jdbcTemplate;
+    @Resource(name = "dynamicDataSource")
+    private DynamicDataSource dataSource;
 
     @Autowired
     private RoutingStrategyFactory routingStrategyFactory;
 
     public long getSeq(String tableName) {
-        try {
+
+        try (Connection connection = dataSource.getConnection();
+             Statement statement = connection.createStatement()) {
             // todo 对接zk
-            // todo 迁移至service
             RoutingStrategy routingStrategy = routingStrategyFactory.getReceiptHandleStrategy("random");
             // 根据当前策略选择数据库
             var dataSourceNo = routingStrategy.selectDb();
             log.debug("当前策略选定的数据源No: {}", dataSourceNo);
-
-            KeyHolder keyHolder = new GeneratedKeyHolder();
+            connection.setAutoCommit(true);
             String sql = "replace into " + tableName + " (stub) values ('0')";
-
             log.debug("执行sql: {}", sql);
-            jdbcTemplate.update(new PreparedStatementCreator() {
-                @Override
-                public PreparedStatement createPreparedStatement(Connection connection) throws SQLException {
-                    PreparedStatement ps = connection.prepareStatement(sql, new String[]{"id"}); // "id" 是主键列的列名
+            statement.executeUpdate(sql, Statement.RETURN_GENERATED_KEYS);
+            ResultSet keys = statement.getGeneratedKeys();
 
-                    return ps;
-                }
-            }, keyHolder);
-            var res = keyHolder.getKeyList().get(0).get("GENERATED_KEY");
-            return (Long) res;
+            if (keys.next()) {
+                long result = keys.getLong(1);
+                log.debug("mysql get last insert id = {}", result);
+                return result;
+            }
+            throw new RuntimeException("replace into error");
+        } catch (SQLException e) {
+            log.error("getSeq error", e);
+            throw new RuntimeException(e);
         } finally {
             // threadLocal 中的数据不用后要即时清理
             DynamicDataSourceContextHolder.clearDateSourceNos();
